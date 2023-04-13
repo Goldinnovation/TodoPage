68f1c142dc893615cb22e300c119f630ea0ebccd
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2023-04-13 22:59:24 +0200

    edit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGODB_URI|| "mongodb://127.0.0.1:27017/TodoPage")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/TodoPage")
 .then(()=> console.log('Database is connected'))
 .catch(error => console.log(error))
 
