b65ee14742ec814191e7c6a3a345089d75a1f144
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2023-04-17 11:35:07 +0200

    edit password

--- Middleware/pwcheck.js
+++ Middleware/pwcheck.js
@@ -3,8 +3,8 @@ export const pwcheck = (req,res,next) => {
    // input checkfield: allows  5 to 15 string with an alphabetiic number at the end
    
      let pw  = req.body.password
-     var specialCheck = pw.match(/[^a-zA-Z0-9]+$/) ? true : false; 
-     if(pw.length > 5 && pw.length < 15 && specialCheck) {
+     var specialCheck = pw.match(/[^a-zA-Z0-9]/) ? true : false; 
+     if(pw.length > 5 && pw.length < 20 && specialCheck) {
         console.log(pw.length)
         return next()
      }else{  
